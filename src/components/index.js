8db47d71d65be0bab087813388bf37ed4fa614cf
Bellage <gehuijun@uw.edu>
2022-11-16 13:40:29 -0800

    new change

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import {renderTaskView} from 'home.js';
-'use strict';
-renderTaskView();
-//you can delete the below line of code
-console.log("If you see this message, the script has loaded!")
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './components/App.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>  
+);
