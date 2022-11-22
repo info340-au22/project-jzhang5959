86fe0df07e73f22bb555283b3ab3adcd93b5e488
jerryiscat <94934251+jerryiscat@users.noreply.github.com>
2022-11-22 15:36:35 -0800

    update the music components

---
+++ src/components/music-play.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function PlayMusic(props) {
+
+
+}
+
+const musicSample = [{musicName:"Beat", mood:"Repulse", audioLink:"", musicType:["rock","quick"]},
+{musicName:"Asad", mood:"Fuming", audioLink:"", musicType:["rock","quick"]},
+{musicName:"fgsraetehf", mood:"Stressed", audioLink:"", musicType:["rock","quick","pop"]}]
+
+const moodSample = [{mood:"Repulse", color:"#EB8075", index:1},
+{mood:"Fuming", color:"#EB8075",index:2},
+{mood:"Stressed", color:"#E55A90",index:3}]
+
+const typeSample = [{musicType:"rock", playPageLink:"/#"},
+{musicType:"quick", playPageLink:"/#"},
+{musicType:"pop", playPageLink:"/#"}]
